2358c3396598826f957a839b08111d2e93150e6a
rajendra-embibe <rajendra@embibe.com>
2017-01-03 17:09:06 +0530

    Update viz.py

--- viz.py
+++ viz.py
@@ -5,19 +5,19 @@ import seaborn as sns
 df = pd.read_csv('firstdata.csv')
 
 
-# fig = plt.figure()
-# ax = fig.add_subplot(1,1,1)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
 
 # making a histogram
-# ax.hist(df['Age'],bins = 10)
-# plt.title('Age Distribution')
-# plt.xlabel('Age')
-# plt.ylabel('Employee')
-# plt.show()
+ax.hist(df['Age'],bins = 10)
+plt.title('Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Employee')
+plt.show()
 
 #boxplot
-# ax.boxplot(df['Age'])
-# plt.show()
+ax.boxplot(df['Age'])
+plt.show()
 
 #violin plot
 sns.violinplot(df['Age'],df['Gender'])
