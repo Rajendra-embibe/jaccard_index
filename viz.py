d56d06521fdef61def4b2265c9583d15078ef664
rajendra-embibe <rajendra@embibe.com>
2017-01-03 17:07:17 +0530

    Added a new file for visualization

---
+++ viz.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+df = pd.read_csv('firstdata.csv')
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+
+# making a histogram
+# ax.hist(df['Age'],bins = 10)
+# plt.title('Age Distribution')
+# plt.xlabel('Age')
+# plt.ylabel('Employee')
+# plt.show()
+
+#boxplot
+# ax.boxplot(df['Age'])
+# plt.show()
+
+#violin plot
+sns.violinplot(df['Age'],df['Gender'])
+sns.despine()
